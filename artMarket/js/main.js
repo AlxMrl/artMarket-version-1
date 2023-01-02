02e793e61aee7e8467f99d6fe8e581a6512ebbff
AlxMrl <114818557+AlxMrl@users.noreply.github.com>
2023-01-02 18:34:25 +0300

    Add files via upload

---
+++ artMarket/js/main.js
@@ -0,0 +1,8 @@
+function artistBtnFunc() {
+    const artistBtn = document.querySelector('.center-btn2')
+
+    artistBtn.addEventListener('click', () => {
+        alert('К сожалению, функционал для работы художником не готов')
+    })
+}
+artistBtnFunc()
